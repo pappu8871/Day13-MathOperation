63fd5a9b0f68ab91fe50170e62651787984f6740
your username <59005110+pappu8871@users.noreply.github.com>
2021-05-29 18:40:56 +0530

    Added:UC-2

--- MathOperation.java
+++ MathOperation.java
@@ -1,20 +1,30 @@
 package com.practiceproblem;
 
 @FunctionalInterface
-interface IMathOperationUC1 {
-int calculate (int a,int b);	
+interface MathOperation {
+int calculate (int a,int b);
+
+static void Result(int a,int b, String function, MathOperation pappu ) {
+System.out.println("Result of "+function+" is "+ pappu.calculate(a, b));
+}}
+public class MathOperationUC2 {
+
+	public static void main(String[] args) {
+		
+		MathOperation add = (x, y) -> x + y;
+		MathOperation substract = (x, y) -> x - y;
+		MathOperation divide = (x, y) -> x / y;
+		
+//		System.out.println("Addition is " + add.calculate(25, 20));
+//		System.out.println("Substraction is " + substract.calculate(25, 20));
+//	    System.out.println("Division is " + divide.calculate(25, 20));
+		
+		MathOperation.Result(15, 6, "Addition", add);
+		MathOperation.Result(15, 6, "Substraction", substract);
+        MathOperation.Result(15, 6, "Division", divide);
+	
+	}
 }
 
-public class MathOperationUC1 {
-public static void main(String[] args) {
-	
-	IMathOperationUC1 add = (x, y) -> x + y;
-	IMathOperationUC1 substract = (x, y) -> x - y;
-	IMathOperationUC1 divide = (x, y) -> x / y;
-	
-	System.out.println("Addition is " + add.calculate(25, 20));
-	System.out.println("Substraction is " + substract.calculate(25, 20));
-    System.out.println("Division is " + divide.calculate(25, 20));
-}
 
-}
+
