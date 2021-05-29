ec038c022b59d7583198ec745edc0127d4d89820
your username <59005110+pappu8871@users.noreply.github.com>
2021-05-29 18:54:38 +0530

    Added:UC-5,ListEven

--- MathOperation.java
+++ MathOperation.java
@@ -1,25 +1,22 @@
 package com.practiceproblem;
+
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
-import java.util.function.Consumer;
-import java.util.function.Function;
+import java.util.function.Predicate;
 
-public class NumberDoubleUC4 {
+public class NumberLIstEvenUC5 {
 
-	public static void main(String[] args) {
-
-		//create collection
-				List<Integer>myNumberList = new ArrayList<Integer>();
-			    for(int i = 0; i<5; i++) myNumberList.add(i);
-			
-
-			    //Implicit Lambda Function to print double Value
-			    
-			    Function<Integer,Double> toDoubleFunction = n -> n.doubleValue(); 
-			    myNumberList.forEach(n -> {
-					 System.out.println("Math1:forEach Lambda impl Value::" + 
-				toDoubleFunction.apply(n));
-			    });
+       public static void main(String[] args) {
+//  Use collection
+	List<Integer>myNumberList = new ArrayList<Integer>();
+	    for(int i = 0; i<5; i++) myNumberList.add(i);
+	
+	//Implicit Lambda Function to check even
+	    Predicate<Integer> isEvenFunction = n -> n > 0 && n%2 == 0 ;
+	    myNumberList.forEach(n -> {
+	    	System.out.println("Value of: "+n+" check for Even " +isEvenFunction.test(n));
+	    });
+	    
 	}
- }
+
+}
