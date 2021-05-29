51ae929a7f2abe200e3449dd9ee30845ab9cb46f
your username <59005110+pappu8871@users.noreply.github.com>
2021-05-29 18:30:58 +0530

    Added:WelcomeFile

---
+++ MathOperation.java
@@ -0,0 +1,10 @@
+package com.practiceproblem;
+
+public class WelcomeMathOperation {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Welcome Math Operation");
+	}
+
+}
