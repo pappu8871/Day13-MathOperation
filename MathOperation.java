d01839e352f7987644ada54a3f90cee77c124e34
your username <59005110+pappu8871@users.noreply.github.com>
2021-05-29 18:34:35 +0530

    Added:UC-1

--- MathOperation.java
+++ MathOperation.java
@@ -1,10 +1,20 @@
 package com.practiceproblem;
 
-public class WelcomeMathOperation {
+@FunctionalInterface
+interface IMathOperationUC1 {
+int calculate (int a,int b);	
+}
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-System.out.println("Welcome Math Operation");
-	}
+public class MathOperationUC1 {
+public static void main(String[] args) {
+	
+	IMathOperationUC1 add = (x, y) -> x + y;
+	IMathOperationUC1 substract = (x, y) -> x - y;
+	IMathOperationUC1 divide = (x, y) -> x / y;
+	
+	System.out.println("Addition is " + add.calculate(25, 20));
+	System.out.println("Substraction is " + substract.calculate(25, 20));
+    System.out.println("Division is " + divide.calculate(25, 20));
+}
 
 }
